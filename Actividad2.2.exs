b3635bd0678a6c2053d25cc72a68f7c93ea7853b
Emilia Salazar <emiliasalazar67@gmail.com>
2023-04-29 16:52:43 -0600

    ejercicio 3 y 4 y 5

--- Actividad2.2.exs
+++ Actividad2.2.exs
@@ -1,13 +1,15 @@
 #implementation od the factorial using recursion
 #
 #Ian Holender
+#Emilia salazar
+#Fernanda Osorio
 # 27-04-2023
 #All problems must be solved with tail recursion
 
 
 defmodule Hw.Ariel2 do
 
-  #Ejercicio 1
+  #1. La funci ́on insert toma dos entradas: un nu ́mero n y una lista lst que contiene nu ́meros en orden ascendente. Devuelve una nueva lista con los mismos elementos de lst pero con n insertado en su lugar correspondiente.
   def insert(list, item),
     do: do_insert(list, item, [])
 
@@ -22,15 +24,39 @@ defmodule Hw.Ariel2 do
 
 
 
-  # #Ejercicio 3
-  # def rotate_left(n, lst) do
-  #   len = length(lst)
-  #   shift = rem(n, len)
-  #   shift = len + shift if shift < 0
-  #   Enum.concat(Enum.drop(lst, shift), Enum.take(lst, shift))
-  # end
 
-    #Ejercicio 4
+
+  # 3. La funci ́on rotate-left toma dos entradas: un nu ́mero entero n y una lista lst. Devuelve la lista que resulta de rotar lst un total de n elementos a la izquierda. Si n es negativo, rota hacia la derecha.
+  def rotate_left(n, list),
+      do: do_rotate_left(n, list)
+
+  def rotate_left(n, list) when n == 0,
+    do: list
+
+  defp do_rotate_left(n, [_head | tail]),
+    do: do_rotate_left(n-1, tail ++ [_head])
+
+
+  #4. La funci ́on prime-factors toma un nu ́mero entero n como entrada (n ¿ 0) y devuelve una lista que contiene los factores primos de n en orden ascendente. Los factores primos son los nu ́meros primos que dividen a un nu ́mero de manera exacta. Si se multiplican todos los factores primos se obtiene el nu ́mero original.
+def prime_factors(number),
+  do: do_prime_factors(number, 2, [])
+
+def prime_factors(number) when number == 0,
+  do: 0
+
+defp do_prime_factors(number, div, list) when div * div > number,
+  do: list ++ [number]
+
+defp do_prime_factors(number, div, list) when rem(number, div) == 0,
+  do: do_prime_factors(div(number, div), div, [div | list])
+
+defp do_prime_factors(number, div, list) when rem(number, div) != 0,
+  do: do_prime_factors(number, div + 1, list)
+
+
+
+
+    #5. La funci ́on gcd toma dos nu ́meros enteros positivos a y b como entrada, donde a > 0 y b > 0. Devuelve el ma ́ximo comu ́n divisor (GCD por sus siglas en ingl ́es) de a y b. No se debe usar la funci ́on gcd o similar predefinida.
     def gcd(a, b),
      do: gcd_do(a, b)
 
@@ -44,10 +70,10 @@ defmodule Hw.Ariel2 do
     defp gcd_do(a, b) when b == 0,
      do: a
 
-    defp gcd_do(a, b) when a<b,
+    defp gcd_do(a, b) when a>b,
      do: gcd_do(b, rem(a, b))
 
-    defp gcd_do(a,b) when a>b,
+    defp gcd_do(a,b) when a<b,
       do: gcd_do( rem(b,a),a)
 
 
@@ -62,3 +88,9 @@ def deep_reverse(list), do: do_deep_reverse(list, [])
 defp do_deep_reverse([], res), do: Enum.reverse(res)
 defp do_deep_reverse([head | tail], res), do: do_deep_reverse(tail, [Enum.reverse(head) | res])
 end
+
+
+
+#Ejercico7 La funci ́on insert-anywhere toma dos entradas: un objeto x y una lista lst. Devuelve una nueva lista con todas las formas posibles en que se puede insertar x en cada posici ́on de lst.
+def insert_anywhere(element, list),
+  do: do_insert_anywhere(element, list, pos)
