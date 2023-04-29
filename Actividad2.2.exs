d4b90e0f3bd36244838a59be7a87c9cfb30e08fb
Emilia Salazar <emiliasalazar67@gmail.com>
2023-04-29 13:08:44 -0600

    ej 4

--- Actividad2.2.exs
+++ Actividad2.2.exs
@@ -22,16 +22,43 @@ defmodule Hw.Ariel2 do
 
 
 
-  #Ejercicio 3
-  def rotate_left(n, lst) do
-    len = length(lst)
-    shift = rem(n, len)
-    shift = len + shift if shift < 0
-    Enum.concat(Enum.drop(lst, shift), Enum.take(lst, shift))
-  end
+  # #Ejercicio 3
+  # def rotate_left(n, lst) do
+  #   len = length(lst)
+  #   shift = rem(n, len)
+  #   shift = len + shift if shift < 0
+  #   Enum.concat(Enum.drop(lst, shift), Enum.take(lst, shift))
+  # end
 
-  #Ejercicio 6
-  def deep_reverse do
+    #Ejercicio 4
+    def gcd(a, b),
+     do: gcd_do(a, b)
 
 
+    defp gcd_do(a, b) when a == b,
+     do: a
+
+    defp gcd_do(a, b) when a == 0,
+     do: b
+
+    defp gcd_do(a, b) when b == 0,
+     do: a
+
+    defp gcd_do(a, b) when a<b,
+     do: gcd_do(b, rem(a, b))
+
+    defp gcd_do(a,b) when a>b,
+      do: gcd_do( rem(b,a),a)
+
+
+
+
+
+#Ejercicio 6
+
+
+def deep_reverse(list), do: do_deep_reverse(list, [])
+
+defp do_deep_reverse([], res), do: Enum.reverse(res)
+defp do_deep_reverse([head | tail], res), do: do_deep_reverse(tail, [Enum.reverse(head) | res])
 end
