c2e605473ee02a6310fa181f1aa8620272078cbf
Fernanda Osorio <A01026502@tec.mx>
2023-05-21 00:55:09 -0600

    Actividad 3

---
+++ Elixir/Actividad3.exs
@@ -0,0 +1,40 @@
+#    Actividad 3.3 Practicando los lenguajes libres de contexto
+#    BNF y EBNF
+#
+#    Fernanda Osorio, Emilia Salazar e Ian Holender
+#    2022_05_20
+
+ @doc """
+  BNF
+
+  ## Modules
+    <module> ::= <defmodule> | <defmodule> <defmodules>
+    <defmodule> ::= defmodule <module_name> do \n\t<body>\n end
+    <body> ::= <function> | <function> <body>
+    <defmodules> ::= <defmodule> | <defmodule> <defmodules>
+
+
+## Functions
+    <functions> ::= <function> | <function> <functions>
+    <function> ::= <function_name> <function_body>
+    <function> ::= def <function_name> <function_arguments> do <statements> end
+
+
+  """
+
+  defmodule Modulos do
+
+    def modulos do
+      "Module" ++ " " ++ "Name" ++ " " ++ "Functions" ++ " " ++ "End"
+    end
+
+    defp modulopriv do
+      "Private Module" ++ " " ++ "Name" ++ " " ++ "Functions" ++ " " ++ "End"
+    end
+
+    def moduleshort,  do: "Module" ++ " " ++ "Name" ++ " " ++ "Functions" ++ " " ++ "End"
+
+    defp moduleshortpriv, do: "Private Module" ++ " " ++ "Name" ++ "Functions" ++ " " ++ "End"
+
+
+  end
