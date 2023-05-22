4c05654103acede34063489e40ec0fc20eeeb3f8
Fernanda Osorio <A01026502@tec.mx>
2023-05-22 17:44:52 -0600

    changes

--- tokens02.rkt
+++ tokens02.rkt
@@ -12,7 +12,9 @@ Examples:
 '(float exp)
 > (arithmetic-lexer "45.3 - +34 / none")
 '(var spa)
-Gilberto Echeverria
+Emilia Salazar
+Fernanda Osorio
+Ian Holender
 2023-03-30
 |#
 
@@ -88,6 +90,8 @@ Gilberto Echeverria
             [(or (eq? char #\e) (eq? char #\E))  (values 'e #f)]
             [(char-operator? char) (values 'op 'int)]
             [(eq? char #\space) (values 'spa 'int)]
+            [(eq? char #\( ) (values 'op_par 'int)]
+            [(eq? char #\) ) (values 'close_par 'int)]
             [else (values 'inv #f )])]
 
     ['dot (cond
@@ -125,18 +129,22 @@ Gilberto Echeverria
             [(eq? char #\space) (values 'spa 'var)]
             [else (values 'inv #f )])]
 
-        ['op (cond
+    ['op (cond
            [(char-numeric? char) (values 'int 'op)]
            [(or (eq? char #\+) (eq? char #\-)) (values 'sign 'op)]
            [(char-alphabetic? char) (values 'var 'op)]
            [(eq? char #\_) (values 'var 'op)]
            [(eq? char #\space) (values 'op_spa 'op)]
+           [(eq? char #\( ) (values 'op_par 'op)]
+           [(eq? char #\) ) (values 'close_par 'op)]
            [else (values 'inv #f)])]
 
     ['spa (cond
             [(char-operator? char) (values 'op #f )]
             [(eq? char #\space) (values 'spa #f )]
-            [else (values 'inv #f )])]
+            [(eq? char #\( ) (values 'op_par #f )]
+            [(eq? char #\) ) (values 'close_par #f)]
+            [else (values 'inv #f)])]
 
     ['op_spa (cond
             [(char-numeric? char) (values 'int #f )]
@@ -180,7 +188,7 @@ Gilberto Echeverria
     ; If the current symbol represents a closing parenthesis, we update the counter and add its token to the list.
     ['close_par (cond
                 [(eq? char #\) ) (values 'close_par 'close_par)]
-                ;;; [(or (eq? char #\+) (eq? char #\-)) (values 'sign 'close_par)]
+                [(or (eq? char #\+) (eq? char #\-)) (values 'sign 'close_par)]
                 [(char-numeric? char) (values 'int 'close_par)]
                 [(char-alphabetic? char) (values 'var 'close_par)]
                 [(char-operator? char) (values 'op 'close_par)]
@@ -191,50 +199,13 @@ Gilberto Echeverria
                 [else (values 'inv #f)])]
 
     
+))
 
-)
-
-        ;;; (define (display-tokens lst)
-        ;;; (cond
-        ;;;     [(null? lst) 'done]
-        ;;;     [else 
-        ;;;     (let ([token (car lst)]
-        ;;;         [rest (cdr lst)])
-        ;;;     (display-token token)
-        ;;;     (display-tokens rest))]))
-
-        ;;; (define (display-token token)
-        ;;; (let ([tipo (get-token-type token)])
-        ;;;     (display (token->string token))
-        ;;;     (display (make-string (- 15 (string-length (token->string token))) #\space))
-        ;;;     (display tipo)
-        ;;;     (newline)))
-
-        ;;; (define (get-token-type token)
-        ;;; (cond
-        ;;;     [(eq? (token-type token) 'int) "Entero"]
-        ;;;     [(eq? (token-type token) 'var) "Variable"]
-        ;;;     [(eq? (token-type token) 'sign) "Operador"]
-        ;;;     [(eq? (token-type token) 'dot) "Punto"]
-        ;;;     [(eq? (token-type token) 'float) "Float"]
-        ;;;     [(eq? (token-type token) 'slash) "División"]
-        ;;;     [(eq? (token-type token) 'e) "Real"]
-        ;;;     [(eq? (token-type token) 'e_sign) "Real Signo"]
-        ;;;     [(eq? (token-type token) 'exp) "Exponencial"]
-        ;;;     [(eq? (token-type token) 'open-pa) "Paréntesis de apertura"]
-        ;;;     [(eq? (token-type token) 'close-par) "Paréntesis de cierre"]
-        ;;;     [(eq? (token-type token) 'equal) "Asignación"]
-        ;;;     [(eq? (token-type token) 'comment) "Comentario"]
-        ;;;     [(eq? (token-type token) 'spa) "Espacio"]
-        ;;;     [(eq? (token-type token) 'spa) "Espacio"]
-        ;;;     [(eq? (token-type token) 'inv) "Token inválido"]
-        ;;;     [else "Tipo desconocido"]))
-)
 
 
 ;;; (display-tokens result)
 
-(define result (arithmetic-lexer ")( D = ( a ^ B + 4 + 10 / 9 "))
+(define result (arithmetic-lexer ")( D = (a ^ B + 4 + 10 / 9 "))
 (define result01 (arithmetic-lexer "2"))
 (define result02 (arithmetic-lexer "97 /6 = 2 + 1"))
 (define result03 (arithmetic-lexer "5.23"))
