8aeee7bd6543f934aa0691af07e6fc17259e2417
Emilia Salazar <emiliasalazar67@gmail.com>
2023-05-24 00:02:56 -0600

    frogger

---
+++ Evidencia_final/funciones.py
@@ -0,0 +1,12 @@
+def greet_user(name):
+    greeting = "Hello, " + name + "!"
+    print(greeting)
+    
+    age = 25
+    print("You are", age, "years old.")
+    
+    birth_year = 2023 - age
+    print("You were born in", birth_year)
+
+greet_user("John")
+
--- cesar.exs
+++ cesar.exs
@@ -1,26 +1,60 @@
+# Functions to work with file Input and Output
+# Perform Caesar Cipher on a file
+# Also showing the use of pipe operator
+#
+# Gilberto Echeverria
+# 2023-05-04
+
 defmodule FileIO do
+
+  @doc """
+  Change one character (received as an ascii numerical value) into another that is
+  'offset' distance from the original one
+  """
   def char_shift(char, offset) do
-    # Shifts a character by a given offset
     cond do
       # Check if the letter is uppercase
       65 <= char and char <= 90 -> Integer.mod(char - 65 + offset, 26) + 65
-      # If the letter is lowecase
+      # Check if the letter is lowercase
       97 <= char and char <= 122 -> Integer.mod(char - 97 + offset, 26) + 97
-      # If the char is not a letter
+      # Any other character
       true -> char
     end
   end
 
+  @doc """
+  Change all the characters in a string using the offset
+  """
   def line_shift(line, offset) do
-    # Shifts a list of characters by a given offset Map uses the function and its arrity (number of
-    # args it recieves), it takes each item of a list to use it as the argument of another function.
-    # Example of map Enum.map([1,2,3], &:math.sqrt/1)
-    to_string(Enum.map(to_charlist(line), fn char -> char_shift(&1, offset) end))
-  end
-end
+    # Using a lambda function for the map
+    #to_string(Enum.map(to_charlist(line), fn char -> char_shift(char, offset) end))
+    # Using capture operator for the map
+    #to_string(Enum.map(to_charlist(line), &char_shift(&1, offset)))
 
-IO.puts(FileIO.char_shift(90, 5))
-IO.puts(FileIO.char_shift(100, 5))
-IO.puts(FileIO.char_shift(89, 5))
-IO.puts(FileIO.char_shift(70, 5))
-IO.puts(FileIO.line_shift("HelloWorld", 15))
+    # Broken down in steps to use logical sequence
+    #temp1 = to_charlist(line)
+    #temp2 = Enum.map(temp1, &char_shift(&1, offset))
+    #to_string(temp2)
+
+    # Using pipe operator to apply the sequence of functions
+    line
+    |> to_charlist()
+    |> Enum.map(&char_shift(&1, offset))
+    |> to_string()
+  end
+
+  @doc """
+  Cipher a complete file using Caesar encoding
+  """
+  def caesar_cipher(in_filename, out_filename, offset) do
+    # Generate the new ciphered data
+    data = in_filename
+           |> File.stream!()
+           |> Enum.map(&line_shift(&1, offset))
+           |> Enum.join("")
+
+    # Store the string in the file
+    File.write(out_filename, data)
+  end
+
+end
