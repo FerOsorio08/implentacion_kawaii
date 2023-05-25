f066a1f73b44fca2f385b907c07ff3daefc3d7c6
Fernanda Osorio <A01026502@tec.mx>
2023-05-25 10:51:03 -0600

    elix

--- Exam_review.exs
+++ Exam_review.exs
@@ -2,4 +2,8 @@ defmodule ExamReview do
   def double(lst), do: double2(lst, [], 1)
   defp double2([], res, n), do: Enum.reverse(res)
   defp double2([head|tail],res,n), do: double2(tail, [head|head * n], n+1)
+
+  def ends4(list), do: do_ends4(list,[])
+  defp do_ends4([],res), do: Enum.reverse(res)
+  defp do_ends4([head|tail],res), when rem(head,10)==4, do: do_ends4([ | tail], res) do: do_ends4(tail,res)
 end
