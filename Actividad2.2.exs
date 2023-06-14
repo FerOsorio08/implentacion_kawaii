466894c878e7906de106a51daddd55882ca52451
Fernanda Osorio <A01026502@tec.mx>
2023-06-14 11:02:31 -0600

    parallel

--- Actividad2.2.exs
+++ Actividad2.2.exs
@@ -1,15 +1,15 @@
-#implementation od the factorial using recursion
+# implementation od the factorial using recursion
 #
-#Ian Holender
-#Emilia salazar
-#Fernanda Osorio
+# Ian Holender
+# Emilia salazar
+# Fernanda Osorio
 # 27-04-2023
-#All problems must be solved with tail recursion
-
+# All problems must be solved with tail recursion
 
 defmodule Hw.Ariel2 do
-
-  #1. La funci ́on insert toma dos entradas: un nu ́mero n y una lista lst que contiene nu ́meros en orden ascendente. Devuelve una nueva lista con los mismos elementos de lst pero con n insertado en su lugar correspondiente.
+  # 1. La funcion insert toma dos entradas: un numero n y una lista lst
+  #que contiene numeros en orden ascendente. Devuelve una nueva lista con los mismos elementos de lst
+  #ero con n insertado en su lugar correspondiente.
   def insert(list, item),
     do: do_insert(list, item, [])
 
@@ -22,73 +22,63 @@ defmodule Hw.Ariel2 do
   defp do_insert([head | tail], item, smaller),
     do: do_insert(tail, item, [head | smaller])
 
-
-
-
-
-  # 3. La funci ́on rotate-left toma dos entradas: un nu ́mero entero n y una lista lst. Devuelve la lista que resulta de rotar lst un total de n elementos a la izquierda. Si n es negativo, rota hacia la derecha.
+  # 3. La funcion rotate-left toma dos entradas: un numero entero n y una lista lst.
+  # Devuelve la lista que resulta de rotar lst un total de n elementos a la izquierda.
+  # Si n es negativo, rota hacia la derecha.
   def rotate_left(n, list),
-      do: do_rotate_left(n, list)
+    do: do_rotate_left(n, list)
 
   defp do_rotate_left(0, list),
-     do: list
+    do: list
 
   defp do_rotate_left(n, [head | tail]),
-    do: do_rotate_left(n-1, tail ++ [head])
+    do: do_rotate_left(n - 1, tail ++ [head])
 
+  # 4. La funcion prime-factors toma un nu ́mero entero n como entrada (n ¿ 0)
+  #y devuelve una lista que contiene los factores primos de n en orden ascendente.
+  #Los factores primos son los nu ́meros primos que dividen a un nu ́mero de manera exacta.
+  #Si se multiplican todos los factores primos se obtiene el nu ́mero original.
+  def prime_factors(number),
+    do: do_prime_factors(number, 2, [])
 
+  defp do_prime_factors(number, div, list) when div * div > number,
+    do: list ++ [number]
 
-  #4. La funci ́on prime-factors toma un nu ́mero entero n como entrada (n ¿ 0) y devuelve una lista que contiene los factores primos de n en orden ascendente. Los factores primos son los nu ́meros primos que dividen a un nu ́mero de manera exacta. Si se multiplican todos los factores primos se obtiene el nu ́mero original.
-def prime_factors(number),
-  do: do_prime_factors(number, 2, [])
+  defp do_prime_factors(number, div, list) when rem(number, div) == 0,
+    do: do_prime_factors(div(number, div), div, [div | list])
 
-defp do_prime_factors(number, div, list) when div * div > number,
-  do: list ++ [number]
+  defp do_prime_factors(number, div, list) when rem(number, div) != 0,
+    do: do_prime_factors(number, div + 1, list)
 
-defp do_prime_factors(number, div, list) when rem(number, div) == 0,
-  do: do_prime_factors(div(number, div), div, [div | list])
+  # 5. La funcion gcd toma dos nu ́meros enteros positivos a y b como entrada,
+  #donde a > 0 y b > 0. Devuelve el maximo comu ́n divisor (GCD por sus siglas en ingl ́es) de a y b.
+  # No se debe usar la funci ́on gcd o similar predefinida.
+  def gcd(a, b),
+    do: gcd_do(a, b)
 
-defp do_prime_factors(number, div, list) when rem(number, div) != 0,
-  do: do_prime_factors(number, div + 1, list)
+  defp gcd_do(a, b) when a == b,
+    do: a
 
+  defp gcd_do(a, b) when a == 0,
+    do: b
 
+  defp gcd_do(a, b) when b == 0,
+    do: a
 
+  defp gcd_do(a, b) when a > b,
+    do: gcd_do(b, rem(a, b))
 
-    #5. La funci ́on gcd toma dos nu ́meros enteros positivos a y b como entrada, donde a > 0 y b > 0. Devuelve el ma ́ximo comu ́n divisor (GCD por sus siglas en ingl ́es) de a y b. No se debe usar la funci ́on gcd o similar predefinida.
-    def gcd(a, b),
-     do: gcd_do(a, b)
+  defp gcd_do(a, b) when a < b,
+    do: gcd_do(rem(b, a), a)
 
+  # Ejercicio 6
 
-    defp gcd_do(a, b) when a == b,
-     do: a
+  def deep_reverse(list), do: do_deep_reverse(list, [])
 
-    defp gcd_do(a, b) when a == 0,
-     do: b
+  defp do_deep_reverse([], res), do: res
 
-    defp gcd_do(a, b) when b == 0,
-     do: a
+  defp do_deep_reverse([head | tail], res) when is_list(head),
+    do: do_deep_reverse(tail, [Enum.reverse(head) | res])
 
-    defp gcd_do(a, b) when a>b,
-     do: gcd_do(b, rem(a, b))
-
-    defp gcd_do(a,b) when a<b,
-      do: gcd_do( rem(b,a),a)
-
-
-
-
-
-#Ejercicio 6
-
-
-def deep_reverse(list), do: do_deep_reverse(list, [])
-
-defp do_deep_reverse([], res), do: res
-defp do_deep_reverse([head | tail], res) when is_list(head), do: do_deep_reverse(tail, [Enum.reverse(head) | res])
-defp do_deep_reverse([head | tail], res), do: do_deep_reverse(tail, [head | res])
-
-
-
-
-
+  defp do_deep_reverse([head | tail], res), do: do_deep_reverse(tail, [head | res])
 end
--- Elixir/paralelo.exs
+++ Elixir/paralelo.exs
@@ -10,8 +10,10 @@ defmodule Hw.Primes do
 
   defp prime(_), do: false
 
+  def sum_primes(limit) when limit < 2, do: 0
   def sum_primes(limit) do
-    Enum.sum(for n <- 2..limit, prime(n), do: n)
+    primes = for n <- 2..limit, prime(n), do: n
+    Enum.sum(primes)
   end
 
   def sum_primes_parallel(limit, num_threads) do
@@ -40,10 +42,33 @@ defmodule Hw.Primes do
       |> IO.inspect()
 
     # Wait for all the tasks to complete and collect their results.
-    #Enum.flat_map flattnes the list of lists
+    # Enum.flat_map flattnes the list of lists
     primes = tasks |> Enum.flat_map(&Task.await/1)
 
     # Sum the primes
     Enum.sum(primes)
   end
+
+  def sum_primes_parallel2(limit, threads) do
+    # Prints out a message indicating the "MAIN THREAD STARTING"
+    IO.puts("MAIN THREAD STARTING")
+
+    1..limit
+    |> Enum.chunk_every(div(limit, threads))
+    # Maps each element in the list of tuples to create a task which asynchronously calls the function given by range_sum passing in the current tuple.
+    |> Enum.map(&Task.async(fn -> sum_primes(&1) end))
+
+    # Outputs the results to the console
+    |> IO.inspect()
+
+    # Waits for completion of all asynchronous tasks
+    |> Enum.map(&Task.await(&1))
+
+    # Outputs the results to the console
+    |> IO.inspect()
+
+    # Prints out a message indicating "FINISHED MAIN THREAD"
+    IO.puts("FINISHED MAIN THREAD")
+  end
+
 end
