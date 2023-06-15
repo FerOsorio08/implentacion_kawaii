aad1060d19d105933a7f63a27107451bf8d6220d
Fernanda Osorio <A01026502@tec.mx>
2023-06-14 19:33:00 -0600

    auxilio ya parallel2

--- Elixir/paralelo.exs
+++ Elixir/paralelo.exs
@@ -11,6 +11,7 @@ defmodule Hw.Primes do
   defp prime(_), do: false
 
   def sum_primes(limit) when limit < 2, do: 0
+
   def sum_primes(limit) do
     primes = for n <- 2..limit, prime(n), do: n
     Enum.sum(primes)
@@ -49,26 +50,56 @@ defmodule Hw.Primes do
     Enum.sum(primes)
   end
 
+  def make_ranges(limit, threads) do
+    # Creates a list of tuples, in this case a hardcoded example
+    per_thread = div(limit, threads)
+    residue = rem(limit, threads)
+
+    ranges =
+      for i <- 0..(threads - 1) do
+        if i == threads - 1 do
+          if residue != 0 do
+            {i * per_thread, (i + 1) * per_thread + residue}
+          else
+            {i * per_thread, (i + 1) * per_thread}
+          end
+        else
+          {i * per_thread, (i + 1) * per_thread}
+        end
+      end
+
+    ranges
+  end
+
+  def prime_sum(limit) do
+    Enum.filter(1..limit, &prime/1)
+    |> Enum.sum()
+  end
+
   def sum_primes_parallel2(limit, threads) do
     # Prints out a message indicating the "MAIN THREAD STARTING"
     IO.puts("MAIN THREAD STARTING")
 
-    1..limit
-    |> Enum.chunk_every(div(limit, threads))
+    make_ranges(limit, threads)
+    # |> Enum.chunk_every(div(limit, threads))
     # Maps each element in the list of tuples to create a task which asynchronously calls the function given by range_sum passing in the current tuple.
-    |> Enum.map(&Task.async(fn -> sum_primes(&1) end))
+    |>IO.inspect()
+    |> Enum.map(&Task.async(fn -> prime_sum(&1) end))
 
     # Outputs the results to the console
-    |> IO.inspect()
+    # |> IO.inspect()
 
-    # Waits for completion of all asynchronous tasks
-    |> Enum.map(&Task.await(&1))
+    # # Waits for completion of all asynchronous tasks
+    # |> Enum.map(&Task.await(&1))
 
-    # Outputs the results to the console
-    |> IO.inspect()
+    # # Outputs the results to the console
+    # |> IO.inspect()
 
     # Prints out a message indicating "FINISHED MAIN THREAD"
     IO.puts("FINISHED MAIN THREAD")
   end
 
+
 end
+
+IO.inspect(Hw.Primes.sum_primes_parallel2(8,3))
